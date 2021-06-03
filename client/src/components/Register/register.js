c4978078305f9c76fd4438b9fc993ed167ec5d3b
Alex Phan <Alexphan1024@gmail.com>
2021-06-02 21:16:29 -0700

    add set/get to form

--- client/src/components/Register/register.js
+++ client/src/components/Register/register.js
@@ -1,19 +1,24 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from "react-router-dom";
 // import * as ReactBootstrap from "react-bootstrap";
-// import { Form, Col, Button, Card } from "react-bootstrap";
+import { Form } from "react-bootstrap";
 
 const Register = () => {
-  const [message, setMessage] = useState("no message :(");
-  useEffect(()=> {
-    fetch('/api/example')
-      .then(res => res.json())
-      .then(resp => setMessage(resp.message))
-  }, [])
+  const [fName, setFName] = useState("");
+  const [lName, setLName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [cPassword, setCPassword] = useState("");
+
+  const handleSubmit = (evt) => {
+    evt.preventDefault();
+    console.log(fName, lName, email, password, cPassword);
+    }
 
   return(
     <div style={{marginTop:'4em'}} className="container-fluid">
-        <div class="card mx-auto" style={{width: '50%'}}>
+        <Form>
+            <div class="card mx-auto" style={{width: '50%'}}>
          <div class="card-body p-40">
             <div class="row">
                 <div class="col-lg-12 text-center">
@@ -23,27 +28,37 @@ const Register = () => {
                 <div class="row mt-4">
                     <div class="col-lg-6">
                         <div class="form-group">
-                            <input type="text" class="form-control" id="InputFirstName" placeholder="First Name"/>
+                            <input type="text" class="form-control" id="InputFirstName" placeholder="First Name"
+                             value={fName}
+                             onChange={e => setFName(e.target.value)}/>
                         </div>
                     </div>
                     <div class="col-lg-6">
                         <div class="form-group">
-                            <input type="text" class="form-control" id="InputLastName" placeholder="Last Name"/>
+                            <input type="text" class="form-control" id="InputLastName" placeholder="Last Name"
+                             value={lName}
+                             onChange={e => setLName(e.target.value)}/>
                         </div>
                     </div>
                 </div>
                 <div class="form-group">
-                    <input type="email" class="form-control" id="InputEmail" placeholder="Email"/>
+                    <input type="email" class="form-control" id="InputEmail" placeholder="Email"
+                     value={email}
+                     onChange={e => setEmail(e.target.value)}/>
                 </div>
                 <div class="row">
                     <div class="col-lg-6">
                         <div class="form-group">
-                            <input type="password" class="form-control" id="InputPassword1" placeholder="Password"/>
+                            <input type="password" class="form-control" id="InputPassword1" placeholder="Password"
+                             value={password}
+                             onChange={e => setPassword(e.target.value)}/>
                         </div>
                     </div>
                     <div class="col-lg-6">
                         <div class="form-group">
-                            <input type="password" class="form-control" id="InputPassword2" placeholder="Confirm Password"/>
+                            <input type="password" class="form-control" id="InputPassword2" placeholder="Confirm Password"
+                             value={cPassword}
+                             onChange={e => setCPassword(e.target.value)}/>
                         </div>
                     </div>
                 </div>
@@ -55,12 +70,13 @@ const Register = () => {
                         </div>
                     </div>
                 </div>
-                <button type="submit" class="btn btn-primary btn-floating btn-lg btn-block m-t-30">Create Your Account</button>
+                <button type="submit" onClick={handleSubmit} class="btn btn-primary btn-floating btn-lg btn-block m-t-30">Create Your Account</button>
                 <Link class="btn btn-warning btn-floating btn-lg btn-block m-t-30" to="/login">Login</Link>
                 {/* <button type="" class="btn btn-warning btn-floating btn-lg btn-block m-t-30">Sign In</button> */}
             </div>
             <Link class="mb-4 ml-4" to="/">Back to Home Page</Link>
     </div>
+        </Form>
     </div>
   );
 }
