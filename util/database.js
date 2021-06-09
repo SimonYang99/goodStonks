a4d447ecd9c922eadd0ca0a36a3407c95ba3c28f
bkwang <bkwang015@outlook.com>
2021-06-08 22:27:50 -0700

    committing my changes to the database files

--- util/database.js
+++ util/database.js
@@ -1,11 +1,12 @@
-const mysql = require('pg');
+const { Client } = require('pg');
 
 // PostGres
-const poolRemote = mysql.createPool ({
+const client = new Client ({
     host: "ec2-34-193-113-223.compute-1.amazonaws.com",
     user: "ijxpcunzisdzwe",
     database: "d37cgb2g46cvj7",
     password: "65a1ca72fe1c333124da8765e544a039c39f6ffca9ba1e89932d1904938ae225",
+    ssl: true,
 });
 
-module.exports = pool.promise();
+module.exports = client.connect();
