a97abb37411e0967a05e3a629351427eed0dd374
Siming <yangsiming1999@gmail.com>
2021-06-11 11:20:55 -0700

    added basic functionality to front-end login/register

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import React, { useState, useLayoutEffect, useMemo } from 'react';
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Login from './components/Login/login';
@@ -8,26 +8,48 @@ import Main from './components/Main/main';
 import Header from './components/Header/header';
 import Profile from './components/Profile/profile';
 
+import { UserProvider } from './context/userContext';
+
 const App = () => {
-  const [message, setMessage] = useState("no message :(");
-  useEffect(()=> {
-    fetch('/api/example')
-      .then(res => res.json())
-      .then(resp => setMessage(resp.message))
+  // const [message, setMessage] = useState("no message :(");
+  // useEffect(()=> {
+  //   fetch('/api/example')
+  //     .then(res => res.json())
+  //     .then(resp => setMessage(resp.message))
+  // }, [])
+
+  const [userInfo, setUserInfo] = useState({user: undefined, loggedIn: false})
+
+  const userValue = useMemo(() => ({
+    userInfo,
+    setUserInfo
+  }), [userInfo])
+
+  useLayoutEffect(() => {
+    let storedUser = JSON.parse(localStorage.getItem("userInfo"));
+    setUserInfo({user: storedUser, loggedIn: true})
   }, [])
 
+  let loginRoute = userInfo.loggedIn
+  ? <></>
+  : <>
+    <Route path="/login" component={Login} />
+    <Route path="/register" component={Register} />
+  </>
+
   return(
-    <BrowserRouter>
-      {/* <h1>{message}</h1> */}
-      <Header />
-      <Switch>
-        <Route exact path="/" component={Main} />
-        <Route path="/test" component={NotHome} />
-        <Route path="/login" component={Login} />
-        <Route path="/register" component={Register} />
-        <Route path="/profile" component={Profile} />
-      </Switch>  
-    </BrowserRouter>
+    <UserProvider value={userValue}>
+      <BrowserRouter>
+        {/* <h1>{message}</h1> */}
+        <Header />
+        <Switch>
+          <Route exact path="/" component={Main} />
+          <Route path="/test" component={NotHome} />
+          <Route path="/profile" component={Profile} />
+          {loginRoute}
+        </Switch>  
+      </BrowserRouter>
+    </UserProvider>
   )
 }
 
