c6ec6277caae849c5ee8abd6d8f00bbe234f1842
Alex Phan <Alexphan1024@gmail.com>
2021-06-11 21:00:01 -0700

    register client side validation

--- client/src/components/Register/register.js
+++ client/src/components/Register/register.js
@@ -10,47 +10,85 @@ const Register = () => {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const [cPassword, setCPassword] = useState("");
+    const [passwordInvalid, setPasswordInvalid] = useState({ message: null, invalid: true });
+    const [fnameInvalid, setFnameInvalid] = useState({ message: null, invalid: true });
+    const [lnameInvalid, setLnameInvalid] = useState({ message: null, invalid: true });
+    const [usernameInvalid, setUsernameInvalid] = useState({ message: null, invalid: true });
+    const [emailInvalid, setEmailInvalid] = useState({ message: null, invalid: true });
+
+    const validateEmail = (email) => {
+        const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        return re.test(String(email).toLowerCase());
+    }
+
+    const validation = () => {
+        let r = true;
+
+        (!!fName) ?
+            setFnameInvalid({ message: null, invalid: false }) :
+            setFnameInvalid({ message: "First Name is required", invalid: true });
+        
+        (!!lName) ?
+            setLnameInvalid({ message: null, invalid: false }) :       
+            setLnameInvalid({ message: "Last Name is required", invalid: true });
+
+        (!!username) ?
+            setUsernameInvalid({ message: null, invalid: false }) :
+            setUsernameInvalid({ message: "Username is required", invalid: true });
+
+        (!!validateEmail(email)) ?
+            setEmailInvalid({ message: null, invalid: false }) :
+            setEmailInvalid({ message: "Email is Invalid", invalid: true });
+
+        (!!(password == cPassword) && !!password && !!cPassword) ?
+            setPasswordInvalid("") :
+            setPasswordInvalid({ message: "Password does not match.", invalid: true });
+
+        (!passwordInvalid.invalid && !fnameInvalid.invalid
+            && !lnameInvalid.invalid && !usernameInvalid.invalid
+            && !emailInvalid.invalid) ? r = true : r = false;
+
+        return r;
+    }
 
     const history = useHistory();
-
 
     let handleSubmit = async (evt) => {
         evt.preventDefault();
 
-        console.log('TESTING: ' + username);
+        if (!!validation()) {
+            var personObject = {
+                title: 'Register new User',
+                username: username,
+                fName: fName,
+                lName: lName,
+                email: email,
+                password: password,
+                cPassword: cPassword
+            }
 
-        var personObject = {
-            title: 'Register new User',
-            username: username,
-            fName: fName,
-            lName: lName,
-            email: email,
-            password: password,
-            cPassword: cPassword
+            var url = '/register'
+
+            // Simple POST request with a JSON body using fetch
+            const requestOptions = {
+                method: 'POST',
+                // mode: 'cors',
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json',
+                    // 'Access-Control-Allow-Origin': url,
+                },
+                body: JSON.stringify(personObject),
+            };
+            await fetch(url, requestOptions)
+                .then(response => response.json())
+                .then(data => {
+                    console.log(data);
+                    //verification stuff
+                    history.push('/login');
+                })
         }
 
-        console.log('TESTING2: ' + JSON.stringify(personObject));
-
-        var url = 'http://localhost:5000/register'
-
-        // Simple POST request with a JSON body using fetch
-        const requestOptions = {
-            method: 'POST',
-            // mode: 'cors',
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json',
-                // 'Access-Control-Allow-Origin': url,
-            },
-            body: JSON.stringify(personObject),
-        };
-        await fetch(url, requestOptions)
-            .then(response => response.json())
-            .then(data => {
-                console.log(data);
-                //verification stuff
-                history.push('/login');
-            })
     }
 
     return (
@@ -63,19 +101,23 @@ const Register = () => {
                                 <h3>GoodStonks</h3>
                             </div>
                         </div>
-                        <div class="row mt-4">                     
+                        <div class="row mt-4">
                             <div class="col-lg-6">
                                 <div class="form-group">
                                     <input type="text" name="fName" class="form-control" id="InputFirstName" placeholder="First Name"
                                         value={fName}
                                         onChange={e => setFName(e.target.value)} />
+                                    <span className="text-danger mt-2" >{fnameInvalid.message}</span>
+
                                 </div>
                             </div>
                             <div class="col-lg-6">
                                 <div class="form-group">
                                     <input type="text" name="lName" class="form-control" id="InputLastName" placeholder="Last Name"
                                         value={lName}
-                                        onChange={e => setLName(e.target.value)} />
+                                        onChange={e => { setLName(e.target.value); }} />
+                                    <span className="text-danger mt-2" >{lnameInvalid.message}</span>
+
                                 </div>
                             </div>
                         </div>
@@ -85,6 +127,8 @@ const Register = () => {
                                     <input type="text" name="username" class="form-control" id="InputUserName" placeholder="Username"
                                         value={username}
                                         onChange={e => setUsername(e.target.value)} />
+                                    <span className="text-danger mt-2" >{usernameInvalid.message}</span>
+
                                 </div>
                             </div>
                             <div class="col-lg-6">
@@ -92,6 +136,7 @@ const Register = () => {
                                     <input type="email" name="email" class="form-control" id="InputEmail" placeholder="Email"
                                         value={email}
                                         onChange={e => setEmail(e.target.value)} />
+                                    <span className="text-danger mt-2" >{emailInvalid.message}</span>
                                 </div>
                             </div>
                         </div>
@@ -100,14 +145,16 @@ const Register = () => {
                                 <div class="form-group">
                                     <input type="password" name="password" class="form-control" id="InputPassword1" placeholder="Password"
                                         value={password}
-                                        onChange={e => setPassword(e.target.value)} />
+                                        onChange={e => { setPassword(e.target.value); }} />
                                 </div>
                             </div>
                             <div class="col-lg-6">
                                 <div class="form-group">
                                     <input type="password" name="cPassword" class="form-control" id="InputPassword2" placeholder="Confirm Password"
                                         value={cPassword}
-                                        onChange={e => setCPassword(e.target.value)} />
+                                        onChange={e => { setCPassword(e.target.value); }} />
+                                    <span className="text-danger mt-2" >{passwordInvalid.message}</span>
+
                                 </div>
                             </div>
                         </div>
